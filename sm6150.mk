d9cc8b4db51ac503b1e310282717a422c40a9ec5
Daniel Rosenberg <drosen@google.com>
2021-07-16 12:52:43 +0200

    sm6150-common: Enable casefolding support on userdata
    This enables casefolding, which is required for removal of sdcardfs.
    
    Test: Wipe data and boot. lsattr /data/media, check for +F
    Bug: 138322712
    Change-Id: I020aa6bb9c121e7bda83c07bc9fb37dcfa26702c

--- sm6150.mk
+++ sm6150.mk
@@ -24,6 +24,9 @@ $(call inherit-product, vendor/xiaomi/sm6150-common/sm6150-common-vendor.mk)
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # Additional native libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
