9cfcca8f8e8cf765bae8c978e7bfb32d6a249720
raul16 <cristian.spataru@gmail.com>
2023-10-28 14:18:32 +0300

    Revert "sm6150-common: Don't inherit updatable_apex.mk"
    This reverts commit 49a68779b828cd6c5760c7d924a4ec870be146a4.

--- sm6150.mk
+++ sm6150.mk
@@ -20,6 +20,9 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/sm6150-common/sm6150-common-vendor.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
