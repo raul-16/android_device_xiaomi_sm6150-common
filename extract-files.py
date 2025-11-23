4d6705c81a266a54108ed1914248a514bff8e1e6
LuK1337 <priv.luk@gmail.com>
2025-11-23 01:24:05 +0100

    sm6150-common: Shim WFD with libinput_shim
    Change-Id: I5c64d4fb6608cf6134ee7647802d9befc26fc8fb

--- extract-files.py
+++ extract-files.py
@@ -46,6 +46,8 @@ lib_fixups: lib_fixups_user_type = {
 
 
 blob_fixups: blob_fixups_user_type = {
+    'system_ext/lib64/libwfdnative.so': blob_fixup()
+        .add_needed('libinput_shim.so'),
     'system_ext/etc/init/wfdservice.rc': blob_fixup()
         .regex_replace(r'(start|stop) wfdservice\b', r'\1 wfdservice64'),
     'system_ext/lib64/libwfdnative.so': blob_fixup()
